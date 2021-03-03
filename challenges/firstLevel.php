4f447caeb5c9bd8a076a4a0076aa50bcaef1e051
Jhonny3e7 <jhon.diaz.3e7@gmail.com>
2021-03-03 11:57:49 -0500

    Se agrega el reto 5, se corrige el espaciado entre retos

--- challenges/firstLevel.php
+++ challenges/firstLevel.php
@@ -1,5 +1,5 @@
 <?php
-/*    // Challenge #1 - "Hola Mundo"
+    // Challenge #1 - "Hola Mundo"
     echo 'Challenge #1 - "Hola Mundo"'.PHP_EOL;
 
     echo "Por favor ingrese su Nombre: ";
@@ -7,6 +7,7 @@
 
     echo PHP_EOL."Hola $name".PHP_EOL;
     echo PHP_EOL.'*********'.PHP_EOL.PHP_EOL;
+
 
     // Challenge #2 - "Hola... nombre y apellido"
     echo 'Challenge #2 - "Hola... nombre y apellido"'.PHP_EOL;
@@ -16,9 +17,9 @@
     echo "Por favor ingrese su Apellido: ";
     $lastName = trim(fgets(STDIN));
 
-
     echo PHP_EOL."Hola, $name $lastName".PHP_EOL;
     echo PHP_EOL.'*********'.PHP_EOL.PHP_EOL;
+
 
     // Challenge #3 - "Mensaje Multilínea"
     echo 'Challenge #3 - "Mensaje Multilínea"'.PHP_EOL;
@@ -26,7 +27,8 @@
     echo 'Platzi cuenta con cursos de '.PHP_EOL.'[MARKETING]'.PHP_EOL.'[DISEÑO Y UX]'.PHP_EOL.'[CRECIMIENTO PROFESIONAL]'.PHP_EOL.'[STARTUPS]'.PHP_EOL.'[DESARROLLO E INGENIERIA]'.PHP_EOL.'[NEGOCIOS Y EMPRENDIMIENTO]'.PHP_EOL.'[CONTENIDO DIGITAL]'.PHP_EOL;
 
     echo PHP_EOL.'*********'.PHP_EOL.PHP_EOL;
-*/
+
+
     // Challenge #4 - "Suma de enteros"
     echo 'Challenge #4 - "Suma de enteros... no tan enteros"'.PHP_EOL;
 
@@ -35,6 +37,20 @@
     echo "Por favor ingrese el segundo número: ";
     $number2 = trim(fgets(STDIN));
 
-
     echo sprintf("El valor de la suma es: %01.2f",($number1+$number2)).PHP_EOL;
     echo PHP_EOL.'*********'.PHP_EOL.PHP_EOL;
+
+
+    // Challenge #5 - "Suma y multiplicación"
+    echo 'Challenge #5 - "Suma y multiplicación"'.PHP_EOL;
+
+    echo "Por favor ingrese el primer número: ";
+    $number1 = trim(fgets(STDIN));
+    echo "Por favor ingrese el segundo número: ";
+    $number2 = trim(fgets(STDIN));
+    echo "Por favor ingrese el tercer número: ";
+    $number3 = trim(fgets(STDIN));
+
+    echo sprintf("El valor de la operación es: %01.2f",(($number1+$number2)*$number3)).PHP_EOL;
+    echo PHP_EOL.'*********'.PHP_EOL.PHP_EOL;
+
